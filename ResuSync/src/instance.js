02e85ed08498e46941f89b5b5fb662a71158149c
swapnil <swapnildhagdi@gmail.com>
2025-10-28 21:02:39 +0530

    add baseUrl

--- ResuSync/src/instance.js
+++ ResuSync/src/instance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance=axios.create({
 
-    baseURL:"",
+    baseURL:"https://resusync-backend.onrender.com",
     timeout:5000,
     headers:{
         'Content-Type':'application/json'
